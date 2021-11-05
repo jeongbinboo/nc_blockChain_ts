60a60025708562fec7b9bb2a86ca9278b78ab723
jeongbinboo <jrkasey3461@naver.com>
2021-11-05 11:45:06 +0900

    done project

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -7,7 +10,54 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+Block.validateStructure = (aBlock) => {
+    return (typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.data === "string" &&
+        typeof aBlock.timestamp === "number");
+};
+Block.calculateBlockHash = (//static으로 메소드를 정의하면 인스턴스 없이 바로 Block.calculateBlockHash 가 가능하다.
+index, previousHash, data, timestamp) => {
+    return (CryptoJS.SHA256(index + previousHash + data + timestamp).toString());
+};
 const genesisBlock = new Block(0, "hello", "", "my name is jboo", 20211105);
 const blockChain = [genesisBlock];
+const getBlockChain = () => blockChain;
+const getLatestBlock = () => blockChain[blockChain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimeStamp = getNewTimeStamp();
+    const nextHash = Block.calculateBlockHash(newIndex, previousBlock.hash, data, newTimeStamp);
+    const newBlock = new Block(newIndex, nextHash, previousBlock.hash, data, newTimeStamp);
+    addBlock(newBlock);
+    return newBlock;
+};
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+    else if (candidateBlock.hash !== Block.calculateBlockHash(candidateBlock.index, candidateBlock.previousHash, candidateBlock.data, candidateBlock.timestamp)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockChain.push(candidateBlock);
+    }
+};
+createNewBlock("hello i am jboo");
+createNewBlock("hihi");
 console.log(blockChain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { time } from 'console';
 import * as CryptoJS from "crypto-js";
+import { create } from 'domain';
 
 class Block{
     public index:number;
@@ -8,13 +9,23 @@ class Block{
     public data:string;
     public timestamp: number;
 
-    static calculateBlockHash = ( //static을 사용해야만 Block.calculateBlockHash가 가능하다
+    static validateStructure = (aBlock:Block) : boolean =>{
+        return(
+            typeof aBlock.index === "number" &&
+            typeof aBlock.hash === "string" &&
+            typeof aBlock.previousHash === "string" &&
+            typeof aBlock.data === "string" &&
+            typeof aBlock.timestamp === "number"
+        )
+    }
+    static calculateBlockHash = ( //static으로 메소드를 정의하면 인스턴스 없이 바로 Block.calculateBlockHash 가 가능하다.
         index:number, 
-        previousHash:string, 
-        timestamp:number, 
-        data:string):string=>
-        CryptoJS.SHA26(index + previousHash + timestamp + data).toString();
-
+        previousHash:string,
+        data:string,
+        timestamp:number
+        ):string => {
+            return (CryptoJS.SHA256(index + previousHash + data + timestamp).toString())
+        }
     constructor(
         index:number,
         hash:string,
@@ -29,10 +40,54 @@ class Block{
         this.timestamp = timestamp;
     }
 }
+
 const genesisBlock:Block = new Block(0,"hello","","my name is jboo",20211105);
+
 const blockChain:Block[] = [genesisBlock];
 
 const getBlockChain = ():Block[] => blockChain;
-const getLatestBlock = () : Block => blockChain[blockChain.length -1];
-const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const getLatestBlock = ():Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = ():number => Math.round(new Date().getTime()/1000);
+
+const createNewBlock = (data:string):Block => {
+    const previousBlock:Block = getLatestBlock();
+    const newIndex:number = previousBlock.index + 1;
+    const newTimeStamp:number = getNewTimeStamp();
+    const nextHash:string = Block.calculateBlockHash(newIndex, previousBlock.hash, data, newTimeStamp);
+    const newBlock:Block = new Block(newIndex, nextHash, previousBlock.hash, data, newTimeStamp);
+    addBlock(newBlock);
+    return newBlock;
+}
+
+const isBlockValid = (candidateBlock : Block, previousBlock:Block):boolean =>{
+    if(!Block.validateStructure(candidateBlock)){
+        return false;
+    }
+    else if(previousBlock.index + 1 !== candidateBlock.index){
+        return false;
+    }
+    else if(previousBlock.hash !== candidateBlock.previousHash){
+        return false;
+    }
+    else if(candidateBlock.hash !== Block.calculateBlockHash(
+        candidateBlock.index, 
+        candidateBlock.previousHash, 
+        candidateBlock.data,
+        candidateBlock.timestamp)){
+            return false;
+        }
+        else{
+            return true;
+        }
+}
+
+const addBlock = (candidateBlock:Block):void =>{
+    if(isBlockValid(candidateBlock,getLatestBlock())){
+        blockChain.push(candidateBlock);
+    }
+}
+createNewBlock("hello i am jboo");
+createNewBlock("hihi");
 console.log(blockChain);
